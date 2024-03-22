8aac225a2f72626ec7b62ab90fbfe42475125442
Célien Heritier <celien.heritier@gmail.com>
2024-03-22 16:09:18 +0100

    fix: microsoft

--- test/pages/login.vue
+++ test/pages/login.vue
@@ -12,7 +12,7 @@ const refresh = () => {
   <div>
     <h1>Login</h1>
     <NuxtLink
-        :to="`${useRuntimeConfig().public.directusUrl}/auth/login/google?redirect=https://test.meet.qualibroker-swissriskcare.com/login`">
+        :to="`${useRuntimeConfig().public.directusUrl}/auth/login/microsoft?redirect=https://test.meet.qualibroker-swissriskcare.com/login`">
       <div
           class="ring ring-2 ring-inset ring-white hover:ring-inter-primary-500 transition bg-primary-900 hover:bg-inter-primary-500 flex gap-4 items-center text-white px-12 py-6 text-2xl justify-center rounded-xl text-inter-primary-500 hover:text-primary-950">
         <i class="fa fal fa-key"></i>
