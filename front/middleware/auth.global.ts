667bf6402e4732a946cca745e19551212fa4581a
Célien Heritier <celien.heritier@gmail.com>
2024-03-25 15:32:21 +0100

    fix: image

--- front/middleware/auth.global.ts
+++ front/middleware/auth.global.ts
@@ -7,18 +7,21 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     useDirectus().init()
   }
 
-  if (!useAuth().user) {
+  // if the user is not logged in, try to refresh the token
+  if(!useAuth().user) {
     try {
       await useDirectus().client.refresh();
       await useDirectus().getUser();
     } catch (e) {
-      // redirect to login
-      if (to.path !== '/login') {
-        return navigateTo('/login')
-      }
     }
   }
 
+  // if the user is still not logged in, redirect to login
+  if (!useAuth().user) {
+    return navigateTo('/login')
+  }
+
+  // if the user is logged in, redirect to home if trying to access login
   if (useAuth().user && to.path === '/login') {
     return navigateTo('/')
   }
